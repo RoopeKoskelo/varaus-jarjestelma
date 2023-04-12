aff3948d5861b6e9202c91ed07d58ff7d5fa07a9
RoopeKoskelo <roope.koskelo@gmail.com>
2023-04-12 11:21:24 +0300

    @aldabil to the rescue kekw

---
+++ react-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Calendar from './components/Calendar';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>yes helo varaus yes</h1>
+      <Calendar></Calendar>
+    </div>
+  );
+}
+
+export default App;
